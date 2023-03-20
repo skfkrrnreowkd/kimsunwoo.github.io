a5ae091e0e30a4680d90572817b3da6c2fd7cca2
김선우 <121792160+skfkrrnreowkd@users.noreply.github.com>
2023-03-20 16:46:06 +0900

    Add files via upload
    간단한 키오스크 만들기

--- Main.java
+++ Main.java
@@ -2,25 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 사용자로부터 숫자를 입력받기 위한 Scanner 객체 생성
         Scanner scanner = new Scanner(System.in);
 
-        // 안내 메시지 출력
-        System.out.println("숫자를 입력하세요:");
+        // 메뉴 출력
+        System.out.println("=== Menu ===");
+        System.out.println("1. Cheeseburger - $5.00");
+        System.out.println("2. Hamburger - $4.00");
+        System.out.println("3. French Fries - $2.50");
+        System.out.println("4. Soda - $1.50");
 
-        // 사용자로부터 숫자 입력받기
-        int num = scanner.nextInt();
+        // 주문 받기
+        System.out.print("Enter menu number: ");
+        int menuNumber = scanner.nextInt();
 
-        // 1부터 입력한 숫자까지의 합 계산
-        int sum = 0;
-        for (int i = 1; i <= num; i++) {
-            sum += i;
-        }
+        // 선택한 메뉴 출력
+        System.out.println("You selected menu " + menuNumber + ".");
 
-        // 결과 출력
-        System.out.println("1부터 " + num + "까지의 합은 " + sum + "입니다.");
-
-        // Scanner 객체 닫기
-        scanner.close();
+        // 주문 완료 메시지 출력
+        System.out.println("Thank you for your order. Please proceed to the payment terminal.");
     }
 }
